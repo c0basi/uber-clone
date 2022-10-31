bf10fcc1a0c7a6a9c2541c8d40493106d30a4d7b
Charles Obasi <charles@trycycledata.com>
2022-10-30 23:35:41 -0400

    Finished redux

--- redux/store.ts
+++ redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import navReducer from "../slices/navSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    nav: navReducer,
+  },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
