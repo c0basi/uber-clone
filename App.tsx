98e76fe943a85fc3fc4e694b8ddc02489d6cbd20
Charles Obasi <charles@trycycledata.com>
2022-11-03 03:16:50 -0400

    Add KeyBoardAvoidingView to stop overlap

--- App.tsx
+++ App.tsx
@@ -1,16 +1,39 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { TailwindProvider } from "tailwind-rn";
+import utilities from "./tailwind.json";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store } from "./src/redux/store";
+import HomeScreen from "./src/screens/HomeScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import RootNavigator from "./src/navigator/RootNavigator";
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Lets build uber</Text>
-        <StatusBar style="auto" />
-      </View>
-    </Provider>
+    <TailwindProvider utilities={utilities}>
+      <Provider store={store}>
+        <NavigationContainer>
+          <SafeAreaProvider>
+            <KeyboardAvoidingView
+              behavior={Platform.OS === "ios" ? "padding" : "height"}
+              style={{ flex: 1 }}
+              keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+            >
+              <RootNavigator />
+              {/* <Text>Lets build uber</Text>
+        <StatusBar style="auto" /> */}
+            </KeyboardAvoidingView>
+          </SafeAreaProvider>
+        </NavigationContainer>
+      </Provider>
+    </TailwindProvider>
   );
 }
 
